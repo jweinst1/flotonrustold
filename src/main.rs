ebcff67857759f116bfafdcaa43a1d51735ec6af
Joshua Weinstein <jweinstein@splunk.com>
2021-05-23 01:38:22 -0700

    second commit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use utils::ptrs;
 mod utils;
 
 fn main() {
-	let b = ptrs::SharedPtr::<i32>{..Default::default() };
+	let b = ptrs::SharedPtr::new(Some(32));
     println!("---- Floton DB ----");
 }
