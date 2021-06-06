6e1a71c3835242175a74a9428fbde27fd2de7aad
Joshua Weinstein <jweinstein@splunk.com>
2021-06-06 09:52:13 -0700

    tally off

--- src/storage.rs
+++ src/storage.rs
@@ -2,12 +2,10 @@ use std::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
 use std::ptr;
 use crate::ptrs::SharedPtr;
 use crate::bit_trie::BitTrie;
-use crate::cache::Cache;
 
 pub struct Storage<T> {
 	items:BitTrie<SharedPtr<T>>,
-	len:AtomicUsize,
-	empty_slots:Cache<SharedPtr<T>>
+	len:AtomicUsize
 }
 
 impl<T> Storage<T> {
