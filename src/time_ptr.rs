8704838c9192ad352cb8b3cb3775a93e75d73c7a
Joshua Weinstein <jweinstein@splunk.com>
2021-07-09 03:30:41 -0700

    time

--- src/time_ptr.rs
+++ src/time_ptr.rs
@@ -1,17 +1,41 @@
 use std::sync::atomic::{AtomicPtr, AtomicUsize, AtomicBool, AtomicU32, AtomicU64, Ordering};
 use std::{thread, ptr};
 use std::time::{Duration, Instant};
+use std::convert::TryFrom;
 
 static MONOTONIC_EPOCH:AtomicPtr<Instant> = AtomicPtr::new(ptr::null_mut());
 
-struct TimePtr<T>(*mut T/*object*/, u64 /*time*/, u8 /*thread id*/);
+// only call once
+pub fn set_epoch() {
+	MONOTONIC_EPOCH.store(Box::into_raw(Box::new(Instant::now())), Ordering::SeqCst);
+}
+
+pub fn destroy_epoch() {
+	let swapped_out = MONOTONIC_EPOCH.swap(ptr::null_mut(), Ordering::SeqCst);
+	unsafe { drop(Box::from_raw(swapped_out)); }
+}
+
+pub fn get_time() -> u64 {
+	unsafe {
+		match MONOTONIC_EPOCH.load(Ordering::SeqCst).as_ref() {
+			// todo, configure precision
+			Some(r) => match u64::try_from(r.elapsed().as_nanos()) {
+				Ok(v) => v,
+				Err(e) => panic!("Could not convert monotonic tick to u64, err: {:?}", e)
+			},
+			None => panic!("MONOTONIC_EPOCH was loaded but not initialized!")
+		}
+	}
+}
+
+pub struct TimePtr<T>(pub *mut T/*object*/, pub u64 /*time*/, pub u8 /*thread id*/);
 
 impl<T> TimePtr<T> {
-    fn make(val:T, tval:u64, tid:u8) -> *mut TimePtr<T> {
-        Box::into_raw(Box::new(TimePtr(Box::into_raw(Box::new(val)), tval, tid)))
+    pub fn make(val:T, tid:u8) -> *mut TimePtr<T> {
+        Box::into_raw(Box::new(TimePtr(Box::into_raw(Box::new(val)), get_time(), tid)))
     }
 
-    fn owned_by(ptr:*mut TimePtr<T>, tid:u8) -> bool {
+    pub fn owned_by(ptr:*mut TimePtr<T>, tid:u8) -> bool {
         unsafe {
             match ptr.as_ref() {
                 Some(r) => r.2 == tid,
@@ -20,7 +44,7 @@ impl<T> TimePtr<T> {
         }
     }
 
-    fn get_time(ptr:*mut TimePtr<T>) -> u64 {
+    pub fn get_time(ptr:*mut TimePtr<T>) -> u64 {
         unsafe {
             match ptr.as_ref() {
                 Some(r) => r.1,
@@ -51,5 +75,21 @@ impl<T> TimePtr<T> {
                    None => false
               }
          }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    //use std::sync::atomic::{AtomicPtr, AtomicI64, Ordering};
+
+    #[test]
+    fn get_time_works() {
+    	set_epoch();
+    	let t1 = get_time();
+    	let t2 = get_time();
+    	assert!(t1 < t2);
+    	destroy_epoch();
     }
 }
