d407e6373bcee7f35ea50d5bc215c469729580e8
Joshua Weinstein <jweinstein@splunk.com>
2021-09-04 12:59:22 -0700

    echo test, switch to non blocking

--- src/tcp.rs
+++ src/tcp.rs
@@ -4,6 +4,7 @@ use std::net::{TcpListener, TcpStream, Shutdown};
 use std::time::Duration;
 use crate::threading::{Switch, TVal, ExecUnitGroup};
 use crate::traits::*;
+use std::io::prelude::*;
 
 
 struct TcpServer {
@@ -16,7 +17,7 @@ struct TcpServer {
 }
 
 impl TcpServer {
-	pub fn new(init_th_count:usize, th_qsize:usize, addr:String, port:u16, func:fn(*mut TcpStream)) -> TcpServer {
+	pub fn new(init_th_count:usize, th_qsize:usize, addr:&String, port:u16, func:fn(*mut TcpStream)) -> TcpServer {
 		let ready = Switch::new();
 		let rswitch = ready.clone();
 		let shut = Switch::new();
@@ -24,6 +25,7 @@ impl TcpServer {
 		let mut egroup = ExecUnitGroup::new(init_th_count, th_qsize, func);
 		let listener = TcpListener::bind((addr.as_str(), port)).unwrap();
 		let tlistener = listener.try_clone().unwrap();
+		tlistener.set_nonblocking(true).expect("Cannot set non-blocking");
 		let handle = thread::spawn(move || {
 			while !rswitch.get() {
 				thread::park_timeout(Duration::from_millis(500));
@@ -41,12 +43,9 @@ impl TcpServer {
 						match egroup.assign_retried(req, 10, Duration::from_millis(100)) {
 							None => {
 								// can't handle it
-								unsafe {
-									req.as_ref().unwrap().shutdown(Shutdown::Both);
-								}
 								free!(req);
 							},
-							Some(_) => ()
+							Some(_) => println!("processed request from {:?}", addr)
 						}
 					},
 					Err(e) => println!("Got error from socket {:?}", e)
@@ -55,7 +54,7 @@ impl TcpServer {
 		});
 		TcpServer{
 			port:port,
-			addr:addr,
+			addr:addr.clone(),
 			core:listener,
 			ready:ready,
 			shutter:shut,
@@ -63,7 +62,7 @@ impl TcpServer {
 		}
 	}
 
-	pub fn start(self) {
+	pub fn start(&self) {
 		assert!(!self.ready.get());
 		self.ready.set(true);
 	}
@@ -74,4 +73,44 @@ impl TcpServer {
 		self.shutter.set(true);
 		self.acceptor.take().unwrap().join().unwrap();
 	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn do_echo(obj:*mut TcpStream) {
+    	unsafe {
+    		let mut buf = [0;4];
+    		let mut robj = obj.as_ref().unwrap();
+    		robj.read(&mut buf);
+    		println!("did the read");
+    		robj.write(&buf);
+    		println!("did the write back");
+    		//robj.shutdown(Shutdown::Both);
+    	}
+    	free!(obj);
+    }
+
+    #[test]
+    fn echo_works() {
+        let serv_addr = String::from("127.0.0.1");
+        let serv_port = 8080;
+        let mut server = TcpServer::new(3, 5, &serv_addr, serv_port, do_echo);
+        server.start();
+        let mut bits = [0;4];
+        let mut resp = [0;4];
+        bits[0] = 4;
+        bits[1] = 5;
+        bits[2] = 88;
+        bits[3] = 55;
+        let mut sock = TcpStream::connect((serv_addr.as_str(), serv_port)).unwrap();
+        sock.write(&bits);
+        sock.read(&mut resp);
+        assert_eq!(resp[0], bits[0]);
+        assert_eq!(resp[1], bits[1]);
+        assert_eq!(resp[2], bits[2]);
+        assert_eq!(resp[3], bits[3]);
+        server.stop();
+    }
 }
