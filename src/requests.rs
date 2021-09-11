91a9c522ab07d885139954c8c8688d6ae8c48265
Joshua Weinstein <jweinstein@splunk.com>
2021-09-10 22:21:18 -0700

    add request flags

--- src/requests.rs
+++ src/requests.rs
@@ -1,12 +1,20 @@
 use std::net::{TcpStream, Shutdown};
 use std::mem;
 use std::io::prelude::*;
+use std::convert::TryInto;
 use crate::traits::*;
 use crate::ports::next_local_addr;
 
 #[derive(Debug, Clone)]
 struct RequestHeader {
-	total_size:u32
+	total_size:u32,
+	flags:u32
+}
+
+impl RequestHeader {
+	fn is_shutdown_req(&self) -> bool {
+		(self.flags & 1) == 1
+	}
 }
 
 #[derive(Debug)]
@@ -17,19 +25,20 @@ struct Request {
 
 impl NewType for Request {
 	fn new() -> Self {
-		Request{header:RequestHeader{total_size:0}, body:Vec::<u8>::new()}
+		Request{header:RequestHeader{total_size:0, flags:0}, body:Vec::<u8>::new()}
 	}
 }
 
 impl Request {
 	fn parse(stream:&mut TcpStream) -> Option<Request> {
 		let mut req = Request::new();
-		let mut size_buf:[u8;4] = [0;4];
-		match stream.read_exact(&mut size_buf) {
+		let mut head_buf:[u8;8] = [0;8];
+		match stream.read_exact(&mut head_buf) {
 			Err(_) => return None,
 			_ => (),
 		}
-		req.header.total_size = u32::from_le_bytes(size_buf);
+		req.header.total_size = u32::from_le_bytes(head_buf[0..4].try_into().unwrap());
+		req.header.flags = u32::from_le_bytes(head_buf[4..8].try_into().unwrap());
 		req.body.resize(req.header.total_size as usize, 0);
 		match stream.read(req.body.as_mut_slice()) {
 			Err(_) => return None,
@@ -52,12 +61,15 @@ mod tests {
 		let mut listener = TcpListener::bind(&addrs[..]).unwrap();
 		let mut client = TcpStream::connect(&addrs[..]).unwrap();
 		let sizer:u32 = 4;
+		let flags:u32 = 1;
 		let bytes:[u8;4] = [55, 22, 33, 44];
 		client.write(&sizer.to_le_bytes()).unwrap();
+		client.write(&flags.to_le_bytes()).unwrap();
 		client.write(&bytes).unwrap();
 		let (mut received, _addr) = listener.accept().unwrap();
 		println!("Got req from {:?}", _addr);
 		let req = Request::parse(&mut received).unwrap();
+		assert!(req.header.is_shutdown_req());
 		assert_eq!(req.header.total_size, 4);
 		assert_eq!(req.body.len(), 4);
 		assert_eq!(req.body[0], bytes[0]);
