431fdbf5c8fd8fcf6c18b7fbbb3cbc962e2ba611
Joshua Weinstein <jweinstein@splunk.com>
2021-09-15 07:30:13 -0700

    make response have flags too

--- src/responses.rs
+++ src/responses.rs
@@ -8,7 +8,8 @@ use crate::logging::*;
 
 #[derive(Debug)]
 struct ResponseHeader {
-	total_size:u32
+	total_size:u32,
+	flags:u32
 }
 
 #[derive(Debug)]
@@ -18,9 +19,9 @@ pub struct Response {
 }
 
 impl Response {
-	pub fn from_vec(output:Vec<u8>) -> Response {
+	pub fn from_vec(output:Vec<u8>, flags:u32) -> Response {
 		let hsize = output.len() as u32;
-		Response{header:ResponseHeader{total_size:hsize}, body:output}
+		Response{header:ResponseHeader{total_size:hsize, flags:flags}, body:output}
 	}
 
 	pub fn size(&self) -> u32 {
@@ -28,8 +29,19 @@ impl Response {
 	}
 
 	pub fn to_tcp_stream(&self, stream:&mut TcpStream) -> bool {
+		let mut resp_head_buf:[u8;8] = [0;8];
+		let size_bytes = self.header.total_size.to_le_bytes();
+		let flag_bytes = self.header.flags.to_le_bytes();
+		resp_head_buf[0] = size_bytes[0];
+		resp_head_buf[1] = size_bytes[1];
+		resp_head_buf[2] = size_bytes[2];
+		resp_head_buf[3] = size_bytes[3];
+		resp_head_buf[4] = flag_bytes[0];
+		resp_head_buf[5] = flag_bytes[1];
+		resp_head_buf[6] = flag_bytes[2];
+		resp_head_buf[7] = flag_bytes[3];
 		loop {
-			match stream.write_all(&self.header.total_size.to_le_bytes()) {
+			match stream.write_all(&resp_head_buf) {
 	    		Ok(_) => break,
 	    		Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => thread::yield_now(),
 	    		Err(e) => { log_error!(Connections, "Got Error for writing response header: {}", e); return false; }
@@ -40,7 +52,7 @@ impl Response {
 			match stream.write_all(self.body.as_slice()) {
 	    		Ok(_) => break,
 	    		Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => thread::yield_now(),
-	    		Err(e) => { println!("Got Error on tcp write {:?}", e); return false; }
+	    		Err(e) => { log_error!(Connections, "Got Error for writing response body {}", e); return false; }
 			}
 		}
 		true
@@ -56,7 +68,7 @@ mod tests {
     #[test]
     fn from_vec_works() {
     	let bytes:Vec<u8> = vec![4, 66, 33, 44];
-    	let resp = Response::from_vec(bytes);
+    	let resp = Response::from_vec(bytes, 0);
     	assert_eq!(resp.size(), 4);
     }
 
@@ -66,20 +78,21 @@ mod tests {
     	let serv_addr = SocketAddr::from(([127, 0, 0, 1], port_to_use));
     	let serv = TcpListener::bind(serv_addr).expect("Could not bind to port");
     	let handle = thcall!({
-    		let mut bits:[u8;8] = [0;8];
+    		let mut bits:[u8;12] = [0;12];
     		let mut client = TcpStream::connect(serv_addr).expect("Could not connect to port");
     		client.read_exact(&mut bits).expect("Could not read response");
     		assert_eq!(4, u32::from_le_bytes(bits[0..4].try_into().unwrap()));
-    		assert_eq!(bits[4], 44);
-    		assert_eq!(bits[5], 33);
-    		assert_eq!(bits[6], 22);
-    		assert_eq!(bits[7], 11);
+    		assert_eq!(0, u32::from_le_bytes(bits[4..8].try_into().unwrap()));
+    		assert_eq!(bits[8], 44);
+    		assert_eq!(bits[9], 33);
+    		assert_eq!(bits[10], 22);
+    		assert_eq!(bits[11], 11);
     	});
 		match serv.accept() {
 			Ok((mut socket, addr)) => {
 				let bytes:Vec<u8> = vec![44, 33, 22, 11];
 				println!("Got request from {:?}", addr);
-				let resp = Response::from_vec(bytes);
+				let resp = Response::from_vec(bytes, 0);
 				assert!(resp.to_tcp_stream(&mut socket));
 			},
 			Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
