628377f6770a70c6aaece9a28d3663094ad34a9e
Joshua Weinstein <jweinstein@splunk.com>
2021-10-08 20:52:26 -0700

    first benchmark written

--- src/benchmark/main.rs
+++ src/benchmark/main.rs
@@ -1,10 +1,42 @@
 #![allow(dead_code)]
 #![allow(unused_macros)]
-mod measure_tools;
+#[macro_use] mod measure_tools;
+
+use std::env;
+use std::process;
+use std::time::Instant;
 
 use floton::logging::*;
 use floton::log_always;
+use floton::traits::*;
+use floton::trie::*;
+
+fn int_trie_node() {
+	average_s!(IntTrieNodeGet, 100000, {
+		let node = IntNode::<u32>::new();
+		let _n1 = node.get_seq(5);
+		let _n2 = node.get_seq(6);
+		let _n3 = node.get_seq(12);
+	});
+}
+
+const INT_TRIE_NODE_GET:&'static str = "int_trie_node_get";
+
+fn run_bench(key:&str) {
+	if key == INT_TRIE_NODE_GET {
+		int_trie_node()
+	} else {
+		log_always!(Bench, "Error: The Benchmark \"{}\" is not found!", key);
+		process::exit(2);
+	}
+}
+
 
 fn main() {
-	log_always!(Bench, "Starting!");
+    let cli_args = env::args().collect::<Vec<String>>();
+    let user_args = &cli_args[1..cli_args.len()];
+    log_always!(Bench, "Running the benchmarks: {:?}", user_args);
+    for i in 0..user_args.len() {
+    	run_bench(user_args[i].as_str());
+    }
 }
--- src/benchmark/measure_tools.rs
+++ src/benchmark/measure_tools.rs
@@ -1,5 +1,34 @@
 
 
-pub fn measure() {
-	unimplemented!();
+/**
+ * Runs some code and takes the average time
+ */
+macro_rules! average_s {
+    ($name:ident, $times:expr, $code:block) => {
+        {
+        	log_always!($name, "Running benchmark {:?} times", $times);
+        	let mut total = 0.0;
+        	for i in 0..$times {
+        		let start = Instant::now();
+        		$code
+        		let lap = start.elapsed().as_secs_f64();
+        		log_always!($name, "Trial = {} time in seconds = {}", i, lap);
+        		total += lap;
+        	}
+        	let avg = total / (($times) as f64);
+        	log_always!($name, "Completed, average in seconds = {}", avg);
+        }
+    };
+
+    ($times:expr, $code:block) => {
+        {
+        	let mut total = 0.0;
+        	for _ in 0..$times {
+        		let start = Instant::now();
+        		$code
+        		total += start.elapsed().as_secs_f64();
+        	}
+        	total / (($times) as f64)
+        }
+    };
 }
